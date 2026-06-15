f5d73fa78de11b55807dd22d038602196f4b0fcc
Xqhare <xqhare@web.de>
2026-06-16 01:39:40 +0200

    refactor(tests): migrate end of flag tests

--- tests/parser_tests/end_of_flag.rs
+++ tests/parser_tests/end_of_flag.rs
@@ -1,1 +1,90 @@
-// end_of_flag
+use eshu::{Cli, CliFlag};
+
+#[test]
+fn end_of_flag_marker_no_store_long_flag() {
+    let cli = Cli::new("test-cli")
+        .add_flag(
+            CliFlag::new("a-flag")
+                .with_short_about("Testing text")
+                .with_long_about("Long testing text \n lorem ipsum")
+                .build()
+                .unwrap(),
+        )
+        .with_version("0.0.0")
+        .try_parse_custom(vec![
+            "test".to_string(),
+            "--a-flag".to_string(),
+            "--".to_string(),
+            "RougeArgument".to_string(),
+        ]);
+    assert!(cli.is_ok());
+    let cli = cli.unwrap();
+    assert!(cli.is_flag_entered("a-flag"));
+    assert_eq!(
+        cli.get_stray_positional_args(),
+        &vec!["RougeArgument".to_string()]
+    );
+}
+
+#[test]
+fn end_of_flag_marker_no_store_short_flag() {
+    let cli = Cli::new("test-cli")
+        .add_flag(
+            CliFlag::new("a-flag")
+                .with_flag_char('a')
+                .with_short_about("Testing text")
+                .with_long_about("Long testing text \n lorem ipsum")
+                .build()
+                .unwrap(),
+        )
+        .with_version("0.0.0")
+        .try_parse_custom(vec![
+            "test".to_string(),
+            "-a".to_string(),
+            "--".to_string(),
+            "RougeArgument".to_string(),
+        ]);
+    assert!(cli.is_ok());
+    let cli = cli.unwrap();
+    assert!(cli.is_flag_entered("a-flag"));
+    assert_eq!(
+        cli.get_stray_positional_args(),
+        &vec!["RougeArgument".to_string()]
+    );
+}
+
+#[test]
+fn end_of_flag_marker_no_store_group_flag() {
+    let cli = Cli::new("test-cli")
+        .add_flag(
+            CliFlag::new("a-flag")
+                .with_flag_char('a')
+                .with_short_about("Testing text")
+                .with_long_about("Long testing text \n lorem ipsum")
+                .build()
+                .unwrap(),
+        )
+        .add_flag(
+            CliFlag::new("b-flag")
+                .with_flag_char('b')
+                .with_short_about("Testing text")
+                .with_long_about("Long testing text \n lorem ipsum")
+                .build()
+                .unwrap(),
+        )
+        .with_version("0.0.0")
+        .try_parse_custom(vec![
+            "test".to_string(),
+            "-ba".to_string(),
+            "--".to_string(),
+            "RougeArgument".to_string(),
+        ]);
+    assert!(cli.is_ok());
+    let cli = cli.unwrap();
+    assert!(cli.is_flag_entered("a-flag"));
+    assert!(cli.is_flag_entered("b-flag"));
+    assert_eq!(
+        cli.get_stray_positional_args(),
+        &vec!["RougeArgument".to_string()]
+    );
+}
